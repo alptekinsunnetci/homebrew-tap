7eb1af79acdbf18d3ebd47dc4b06cc7040b6af91
Alptekin Sünnetci <alptekin@sunnetci.net>
2026-04-14 16:16:59 +0300

    Update netplotter.rb

--- Formula/netplotter.rb
+++ Formula/netplotter.rb
@@ -13,9 +13,25 @@ class Netplotter < Formula
     end
   end
 
+  on_linux do
+    url "https://github.com/alptekinsunnetci/netplotter/releases/download/v1.0/netplotter-linux-amd64"
+    sha256 "b3a55fdfa7a46265ceb6690e51561c50a3a2be3c4d367b2b2f0aa2d6cf92eae9"
+  end
+
   def install
-    bin.install Dir["netplotter-*"][0] => "netplotter"
+    asset = Dir["netplotter-*"].first
+    odie "Expected a netplotter-* asset in the build directory" if asset.nil?
+
+    bin.install asset => "netplotter"
     chmod 0755, bin/"netplotter"
+  end
+
+  def caveats
+    <<~EOS
+      Windows: Homebrew formula does not support Windows installs.
+      Please download netplotter-windows-amd64.exe from:
+        https://github.com/alptekinsunnetci/netplotter/releases/tag/v1.0
+    EOS
   end
 
   test do
